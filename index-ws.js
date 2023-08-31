ee938de21c8f0033e7f9f49214dd3574acc987a1
alaqsaka <aqsaabidin15@gmail.com>
2023-08-31 22:02:32 +0700

    Add database logging

--- index-ws.js
+++ index-ws.js
@@ -11,6 +11,16 @@ server.listen(3000, function () {
   console.log(`Server started on port 3000`);
 });
 
+process.on("SIGINT", () => {
+  console.log("sigint");
+  wss.clients.forEach(function each(client) {
+    client.close();
+  });
+  server.close(() => {
+    shutdownDB();
+  });
+});
+
 /** Begin Websocket */
 const WebSocketServer = require("ws").Server;
 
@@ -26,6 +36,10 @@ wss.on("connection", function connection(ws) {
     ws.send("Welcome to my server! - Aqsa");
   }
 
+  db.run(`INSERT INTO visitors (count, time)
+    VALUES (${numClients}, datetime('now'))
+  `);
+
   ws.on("close", function close() {
     wss.broadcast(`Current visitors ${numClients}`);
     console.log("A client has disconnected");
@@ -37,3 +51,32 @@ wss.broadcast = function broadcast(data) {
     client.send(data);
   });
 };
+
+/** End Websockets */
+
+/** begin database */
+const sqlite = require("sqlite3");
+const db = new sqlite.Database(":memory:");
+
+db.serialize(() => {
+  db.run(
+    `
+      CREATE TABLE visitors (
+        count INTEGER,
+        time TEXT
+      )
+    `
+  );
+});
+
+function getCounts() {
+  db.each("SELECT * FROM visitors", (err, row) => {
+    console.log(row);
+  });
+}
+
+function shutdownDB() {
+  getCounts();
+  console.log("Shutting down DB...");
+  db.close();
+}
